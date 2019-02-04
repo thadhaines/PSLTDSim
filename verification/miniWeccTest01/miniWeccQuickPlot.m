8acebf26e7de2514717a9feab186d4d14eb7fc22
thadhaines <jthaines@mtech.edu>
2019-02-03 23:48:58 -0700

    tex adjusts

--- verification/miniWeccTest01/miniWeccQuickPlot.m
+++ verification/miniWeccTest01/miniWeccQuickPlot.m
@@ -8,13 +8,18 @@
 %                       print_f requires altmany export fig
 %
 %   History:
-%   02/03/19    hh:mm   init
+%   02/03/19    14:05   init
+%   02/03/19    14:05   addition of multiple loads and legend
 
 %% init
 clear; format compact; clc; close all; 
 
+%% Global Flags
+makeLegend = 1;
+debug = 0;
+
 %% import LTD data
-dataName = 'miniWeccTest02.mat' % 'plotTest01.mat'%
+dataName = 'miniWeccTest02.mat' %  'plotTest01.mat'%
 % Assumes 1 generator or load per bus... Dictionary does have info available to allow for better looping
 
 load(dataName)
@@ -30,20 +35,39 @@ title('System Frequency')
 hold on
 plot(mir.t,mir.f)
 
-%% plot all load power in all areas
+%% plot all load power in all areas with state == 1
 figure
 title('System P Loading')
 hold on
+legNames = {};
 for area = 1:max(size(mir.areaN)) % for each area
-    fprintf('area %d\n',mir.areaN(area) )
+    if debug
+        fprintf('area %d\n',mir.areaN(area) )
+    end
     curArea = ['A',int2str(area)];
     
-    for load = 1:max(size(mir.(curArea).loadBusN))
-        curLoad = ['L',int2str(mir.(curArea).loadBusN(load))];
-        stairs(mir.t, mir.(curArea).(curLoad).L1.P)
+    for load = 1:max(size(mir.(curArea).loadBusN))-1
+        curLoadbus = ['L',int2str(mir.(curArea).loadBusN(load))];
+        if debug
+            disp(curLoadbus)
+        end
+        for loadOnBus = 1:mir.(curArea).(curLoadbus).Nload
+            if debug
+                disp(loadOnBus)
+            end
+            curLoad =['L',int2str(loadOnBus)];
+            P = mir.(curArea).(curLoadbus).(curLoad).P;
+            St = double(mir.(curArea).(curLoadbus).(curLoad).St);
+            name = [(curArea),'.',(curLoadbus),'.',(curLoad)];
+            legNames{end+1} = name;
+            stairs(mir.t, P.*St)
+        end
     end
 end
-clear area curArea areaBus load curLoad
+if makeLegend
+    legend(legNames)
+end
+clear area curArea areaBus load curLoad P St curLoadbus loadOnBus name legNames
 
 %% plot all gen Pe from all areas
 figure
@@ -63,7 +87,7 @@ for area = 1:max(size(mir.areaN)) % for each area
         stairs(mir.t, mir.(curArea).(curGen).S1.Pe)
     end
 end
-clear area curArea gen curGen
+clear area curArea gen curGen slack
 
 %% plot all gen Q from all areas
 figure
@@ -82,7 +106,7 @@ for area = 1:max(size(mir.areaN)) % for each area
         stairs(mir.t, mir.(curArea).(curGen).S1.Q)
     end
 end
-clear area curArea gen curGen
+clear area curArea gen curGen slack
 
 %% plot all bus voltages from all areas
 figure
@@ -102,8 +126,8 @@ for area = 1:max(size(mir.areaN)) % for each area
         stairs(mir.t, mir.(curArea).(curGen).Vm)
     end
     for load = 1:max(size(mir.(curArea).loadBusN))
-        curLoad = ['L',int2str(mir.(curArea).loadBusN(load))];
-        stairs(mir.t, mir.(curArea).(curLoad).Vm)
+        curLoadbus = ['L',int2str(mir.(curArea).loadBusN(load))];
+        stairs(mir.t, mir.(curArea).(curLoadbus).Vm)
     end
     for xbus = 1:max(size(mir.(curArea).xBusN))
         curXbus = ['x',int2str(mir.(curArea).xBusN(xbus))];
@@ -127,12 +151,12 @@ for area = 1:max(size(mir.areaN)) % for each area
         stairs(mir.t, rad2deg(mir.(curArea).(curGen).Va))
     end
     for load = 1:max(size(mir.(curArea).loadBusN))
-        curLoad = ['L',int2str(mir.(curArea).loadBusN(load))];
-        stairs(mir.t, rad2deg(mir.(curArea).(curLoad).Va))
+        curLoadbus = ['L',int2str(mir.(curArea).loadBusN(load))];
+        stairs(mir.t, rad2deg(mir.(curArea).(curLoadbus).Va))
     end
     for xbus = 1:max(size(mir.(curArea).xBusN))
         curXbus = ['x',int2str(mir.(curArea).xBusN(xbus))];
         stairs(mir.t, rad2deg(mir.(curArea).(curXbus).Va))
     end
 end
-clear area curArea slack gen load xbus curLoad curXbus curGen
+clear area curArea slack gen load xbus curLoad curLoadbus curXbus curGen
