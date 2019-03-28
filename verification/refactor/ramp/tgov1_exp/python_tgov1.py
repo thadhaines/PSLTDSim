0ab996856e738d5978703d8fc7892cc32ca4228d
thadhaines <jthaines@mtech.edu>
2019-03-28 11:00:25 -0600

    Update of tgov work

--- verification/refactor/ramp/tgov1_exp/python_tgov1.py
+++ verification/refactor/ramp/tgov1_exp/python_tgov1.py
@@ -3,15 +3,12 @@ import scipy.signal as sig
 import matplotlib.pyplot as plt
 import scipy.io as sio
 
-# Inputs:
-Pm = .5
+# Inputs
 Pref = .5 # will be a PU of Pref from Generator
 delta_w = 0.0
-Sbase = 100
 
-# Initialization parameters
+# Simulation Parameters
 t = np.linspace(0,30,101)
-
 R = 0.05
 Vmax = 1.0
 Vmin = 0.0
@@ -20,33 +17,40 @@ T2 = 3.0
 T3 = 10.0
 Dt = 0.0
 
+# System Creations
 sys1 = sig.TransferFunction(1,[T1, 1])
 sys2 = sig.TransferFunction([T2, 1],[T3, 1])
 
-# Input
+# Input to system
 u = (Pref-delta_w)/R
 
 # First Block
 _, y1, x1 = sig.lsim2(sys1, [u]*t.size, t)
 
-# limit between Vmax
+# limit Valve position
 for x in range(t.size):
     if y1[x]>Vmax:
         y1[x] = Vmax
     elif y1[x]<Vmin:
-        y1[x] =Vmin
+        y1[x] = Vmin
 
-# second block
-_, y2, x2 = sig.lsim2(sys2, y1,t)
+# Second block
+_, y2, x2 = sig.lsim2(sys2, y1, t)
 
-# addition of delta_w*Dt
-
+# Addition of damping
 y2 = y2 + [delta_w*Dt]*y2.size
 
-# Output is Pmech (as PU
-plt.plot(t,y2)
+# Plot output
+plt.plot(t,y2, label="Pmech Out")
+plt.plot(t,[u*R]*t.size, label="Pref In")
+plt.title('SciPy Simulated Tgov1')
+plt.ylabel(r'$P_{mech}$ [PU]')
+plt.xlabel('Time [sec]')
 plt.grid()
+plt.legend()
 plt.show()
+
+# Output data dictionary as .mat
 pyTgov = {'t_py': t,
           'y_py': y2,
           }
--- verification/refactor/ramp/tgov1_exp/tgov1_test.m
+++ verification/refactor/ramp/tgov1_exp/tgov1_test.m
@@ -26,22 +26,32 @@ load('tgovTest.mat')
 
 figure
 subplot(3,1,1)
-plot(tout,Pmout,'linewidth',1)
-title('MATLAB v Python Tgov1')
+plot(tout,Pmout,'-','linewidth',1.25)
+title('MATLAB vs Python Tgov1 Simulation with Zero Initial Conditions')
 hold on
-plot(t_py, y_py,'--','linewidth',1)
-legend('simulink','python')
+plot(t_py, y_py,'--','linewidth',2)
+
+line([tout(1), tout(end)],[Pref,Pref],'color','k')
+legend('Simulink','SciPy', 'Pref Input')
+set(gca, 'fontsize', 12)
 grid on
 
 subplot(3,1,2)
 abs_dif = abs(y_py'-Pmout);
-plot(tout, abs_dif)
+plot(tout, abs_dif,'.-')
 grid on
 title('Absolute Difference')
+set(gca, 'fontsize', 12)
 
 subplot(3,1,3)
 rel_dif = abs(y_py'-Pmout)./abs(Pmout)*100;
-plot(tout, rel_dif)
+plot(tout, rel_dif,'.-')
 grid on
 ylabel('Percent')
 title('Relative Difference')
+set(gca, 'fontsize', 12)
+
+set(gcf, 'position', [680 454 1093 524])
+
+%set(gcf,'color','w'); % to remove border of figure
+%export_fig('XXXXXX','-pdf');
