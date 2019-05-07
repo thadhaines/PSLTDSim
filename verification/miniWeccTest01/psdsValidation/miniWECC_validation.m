0326253cb4350e53f8928d5cc05c4e57dcd2265f
thadhaines <jthaines@mtech.edu>
2019-05-06 22:02:21 -0600

    update of plotting

--- verification/miniWeccTest01/psdsValidation/miniWECC_validation.m
+++ verification/miniWeccTest01/psdsValidation/miniWECC_validation.m
@@ -65,16 +65,19 @@ x_lim = [mir.t(1), mir.t(end)];
 bfz = 13;
 %% Plot all frequency responses and LTD on top
 figure('position',ppos)
-axes('ColorOrder',colormap(spring)) % to make mess of lines look nicer
-
+axes('ColorOrder',flipud(imcomplement(colormap(spring)))) % to make mess of lines look nicer
 hold on
-plot(mir.t, mir.f, 'k','linewidth',1)
-for freq=1:max(size(f_col))
-    plot(t, psds_data.Data(:,f_col(freq))/60)
+plot(t, psds_data.Data(:,f_col(1))/60,'linewidth',1.5)
+
+
+for freq=2:max(size(f_col)-1)
+    plot(t, psds_data.Data(:,f_col(freq))/60 ,'HandleVisibility','off')
 end
-plot(mir.t, mir.f, 'w','linewidth',2)
-plot(mir.t, mir.f, 'k','linewidth',1)
-legend({'LTD','PSDS'},'location','southeast')
+plot(mir.t, mir.f, 'k:','linewidth',1)
+plot(mir.t, mir.f, 'w','linewidth',3,'HandleVisibility','off')
+plot(t, psds_data.Data(:,f_col(size(f_col,2)))/60,'linewidth',1.5)
+plot(mir.t, mir.f, 'm-.','linewidth',2)
+legend({'PSDS','PSDS','PSDS','LTD'},'location','southeast')
 
 xlim(x_lim)
 grid on
@@ -85,11 +88,12 @@ set(gca,'fontsize',bfz)
 
 %% Plot Average System frequency responses
 figure('position',ppos)
-
-plot(mir.t, mir.f ,'k','linewidth',1 )
+plot(t, fAve,'color', [0 1 0],'linewidth',1.5)
 hold on
-plot(t, fAve,'r','linewidth',1)
-legend({'LTD','PSDS'},'location','southeast')
+
+plot(mir.t, mir.f , 'm-.','linewidth',2)
+
+legend({'PSDS Average','LTD'},'location','southeast')
 xlim(x_lim)
 grid on
 title('Comparison of Average System Frequency')
@@ -102,14 +106,54 @@ relDif = (mir.f - pulledf)./pulledf*100; % as a percent
 
 figure('position',ppos)
 hold on
-plot(mir.t, relDif,'k')
+plot(mir.t, abs(relDif),'ko-.')
 xlim(x_lim)
 grid on
-title('Relative Difference % (PSDS-LTD)')
+title('Absolute Relative Percent Difference (PSDS-LTD)')
 ylabel('Relative Difference [%]')
 xlabel('Time [sec]')
 set(gca,'fontsize',bfz)
 
+%% Experimental pm plotting
+figure
+subplot(2,1,1)
+set(gca,'ColorOrder',colormap(cool))
+set(gca,'ColorOrderIndex',30)
 
+hold on
+for mach=1:max(size(pm_col))
+    plot(t, psds_data.Data(:,pm_col(mach))/psds_data.Data(1,pm_col(mach)) ,'HandleVisibility','off')
+end
+xlim(x_lim)
+title('Normalized PSDS Generator Pm')
+%
+
+subplot(2,1,2)
+set(gca,'ColorOrder',colormap(cool))
+set(gca,'ColorOrderIndex',30)
+hold on
+legNames ={};
+for area = 1:max(size(mir.areaN)) % for each area
+
+    curArea = ['A',int2str(area)];
+    
+    for slack = 1:max(size(mir.(curArea).slackBusN))
+        curSlack = ['S',int2str(mir.(curArea).slackBusN(slack))];
+        plot(mir.t, mir.(curArea).(curSlack).S1.Pm/mir.(curArea).(curSlack).S1.Pm(1))
+        name = [(curArea),'.',(curSlack)];
+        legNames{end+1} = name;
+    end
+    for gen = 1:max(size(mir.(curArea).genBusN))
+        curGen = ['G',int2str(mir.(curArea).genBusN(gen))];
+        % place for for each gen in Ngen...
+        
+        plot(mir.t, mir.(curArea).(curGen).G1.Pm/mir.(curArea).(curGen).G1.Pm(1) )
+        name = [(curArea),'.',(curGen)];
+        legNames{end+1} = name;
+    end
+    %legend(legNames)
+end
+xlim(x_lim)
+title('Normalized LTD Generator Pm')
 %set(gcf,'color','w'); % to remove border of figure
 %export_fig('XXXXXX','-pdf'); % to print fig
