e4006280aeac8ca2bf16282115e591c950c2de4d
thadhaines <45861859+thadhaines@users.noreply.github.com>
2019-11-07 12:34:14 -0700

    work on BAAL plot

--- tests/BAALcalcs/BAALcalc.py
+++ tests/BAALcalcs/BAALcalc.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 Fbase = 60
 Fs = 1*Fbase
 
-Beta = -100 # frequency bias
+betaList = [-50, -100, -150, -200] # frequency bias
 Fa = 60.005 # actual frequency - rolling minute average...
 
 eps1 = 0.0228 # Hz, dictated by interconnection
@@ -19,38 +19,51 @@ f_dev_Low = FTL_low - Fs
 f_dev = Fa-Fs
 
 # if Fs= Fa, BAAL does not apply...?
-BAAL_low = -10*Beta*f_dev_Low*(f_dev_Low/f_dev)
-BAAL_high = -10*Beta*f_dev_High*(f_dev_High/f_dev)
-
-
 
 fRangeL = np.arange(59.96, 59.999, 0.001)
 fRangeH = np.arange(60.001, 60.04, 0.001)
-fRange = np.concatenate((fRangeL , fRangeH), axis=0)
-BAAL_range = np.zeros_like(fRange)
-n = 0
-
-for f in fRange:
-    f_dev = f-Fs
-
-    if f_dev > 0:
-        #print(str(f)+' BAAL high = %.2f' % BAAL_high)
-        BAAL_range[n]= -10*Beta*f_dev_High*(f_dev_High/f_dev)
-    elif f_dev < 0:
-        #print(str(f)+' use BAAL Low = %.2f' % BAAL_low)
-        BAAL_range[n]= -10*Beta*f_dev_Low*(f_dev_Low/f_dev)
-    elif f_dev == 0.0:
-        #print('Frequency match, BAAL does not apply')
-        BAAL_range[n]= 0
-
-    n += 1
-
 fig, ax = plt.subplots()
+colors=[ [0,0,0],
+        [.7,.7,.7],
+        [0,1,0],
+        [1,0,1],
+    ]
+betaV = 0
+
+for Beta in betaList:
+    BAAL_low = -10*Beta*f_dev_Low*(f_dev_Low/f_dev)
+    BAAL_high = -10*Beta*f_dev_High*(f_dev_High/f_dev)
+    makeLegend = True
+
+    for fRange in [fRangeL, fRangeH]:
+        BAAL_range = np.zeros_like(fRange)
+        n = 0
+        for f in fRange:
+            f_dev = f-Fs
+
+            if f_dev > 0:
+                #print(str(f)+' BAAL high = %.2f' % BAAL_high)
+                BAAL_range[n]= -10*Beta*f_dev_High*(f_dev_High/f_dev)
+            elif f_dev < 0:
+                #print(str(f)+' use BAAL Low = %.2f' % BAAL_low)
+                BAAL_range[n]= -10*Beta*f_dev_Low*(f_dev_Low/f_dev)
+            elif f_dev == 0.0:
+                #print('Frequency match, BAAL does not apply')
+                BAAL_range[n]= 0
+
+            n += 1
+        if makeLegend:
+            ax.plot(fRange, BAAL_range,ls='-', label =r'B = '+str(Beta), color =colors[betaV])
+            makeLegend = False
+        else:
+            ax.plot(fRange, BAAL_range,ls='-', color =colors[betaV])
+
+
+    betaV += 1
 # Testing of output (i.e input to gain of Mbase and sum pref)
-ax.plot(fRange, BAAL_range,ls='-', label =r'BAAL', color =[0, 0, 0])
 
 plt.xlim(59.96, 60.04)
-plt.ylim(-1000,1000)
+plt.ylim(-666,666)
 plt.grid(True)
 plt.title('Balancing Authority ACE Limit')
 plt.xlabel('Frequency [Hz]')
@@ -58,7 +71,7 @@ plt.ylabel(r'BAAL [MW]')
 plt.legend()
 
 fig.set_dpi(150)
-fig.set_size_inches(9*.7, 4.5*.85)
+fig.set_size_inches(9*.8, 4.5*.7)
 fig.tight_layout()
 printFigs = True
 if printFigs: plt.savefig('BAAL.pdf', dpi=300)
