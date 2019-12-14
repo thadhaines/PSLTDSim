c6a84c48476b06ffeb64aaa5422bf9b7eb203214
thadhaines <jthaines@mtech.edu>
2019-12-14 05:10:32 -0700

    update of EIA parser to include optional data start, shift, first value match, additional day data read...

--- tests/191213-EIAdataParse/readEIAdata.py
+++ tests/191213-EIAdataParse/readEIAdata.py
@@ -1,13 +1,23 @@
 """
 Read EIA csv data, pull and sort MW load data, and display usable output
+
+Meant to be used to generate input to Load control / Dispatch Control Agents
 """
 ## User specified input data
 # full file Name (assumed in same folder as this file)
-#fileName = "Balancing_authority_hourly_actual_and_forecast_demand_12_per_05_per_2019_–_12_per_12_per_2019_MST (1).csv"
+fileName = "Balancing_authority_hourly_actual_and_forecast_demand_12_per_05_per_2019_–_12_per_12_per_2019_MST (1).csv"
 #fileName = "Balancing_authority_hourly_actual_and_forecast_demand_12_per_05_per_2019_–_12_per_12_per_2019_MST (2).csv"
 fileName = "Balancing_authority_hourly_actual_and_forecast_demand_12_per_05_per_2019_–_12_per_12_per_2019_MST.csv"
 
 dataDate = "12/11/2019" # Date format in csv from EIA
+dataTimeStart = "05" # hour format as in EIA csv
+
+additionalDate = "12/12/2019"
+addDayStop = '14' # time to stop on next day (work around for incomplete data)
+
+shiftTime = True # set shifted time start as 0
+
+firstValMatch = True # Ensure that the first data points match (useful in practice?)
 
 ## script start
 # open file, initialize line outputD, and dataName
@@ -17,7 +27,8 @@ outputD = {}
 dataName = None
 
 # read file, pull data of interest into outputD
-while line:
+for line in reversed(list(open(fileName, 'r'))):
+#while line: # from non-reversed file read
     print(line)
     # get data name from file
     if line.split(',')[0] == 'Category':
@@ -27,9 +38,25 @@ while line:
         mwDemand = line.split()[2].split(',')[1]
         mwForcast = line.split()[2].split(',')[2]
         time = line.split()[1].replace('H','')
-        outputD[time] = { 'demand' : mwDemand, 'forcast': mwForcast}
-    
-    line = next(file,  None) # get next line, if there is one
+        if int(time) >= int(dataTimeStart):
+            if shiftTime:
+                time = str(int(time)-int(dataTimeStart))
+                if int(time) < 10:
+                    time = '0'+time
+            outputD[time] = { 'demand' : mwDemand, 'forcast': mwForcast}
+
+    # Requires file to be read backwards...
+    if additionalDate != None:
+        if line.split()[0] == additionalDate:
+            mwDemand = line.split()[2].split(',')[1]
+            mwForcast = line.split()[2].split(',')[2]
+            time = str( int(time)+1 )            
+            if int(time) < 10:
+                time = '0'+time
+            if int(line.split()[1].replace('H',''))<= int(addDayStop):
+                outputD[time] = { 'demand' : mwDemand, 'forcast': mwForcast}
+        
+    #line = next(file,  None) # get next line, if there is one - from non-reversed file read
 
 file.close() # close file
 
@@ -37,6 +64,7 @@ file.close() # close file
 print("# Data from: %s %s" %(dataDate, dataName))
 outputTuples = ['demand','forcast']
 plotD = {}
+firstVal = None
 
 for name in outputTuples:
     outputList = []
@@ -44,36 +72,59 @@ for name in outputTuples:
     
     print("%s = [ \n\t#(time , MW data)" % name)
     for entry in sorted(outputD):
+        
+        if firstVal == None:
+            firstVal = int(outputD[entry][name])
+
+        if firstValMatch and len(time) == 0:
+            data = firstVal
+        else:
+            data = int(outputD[entry][name]) 
+            
+        outputList.append( data )
         time.append(int(entry))
-        outputList.append( int(outputD[entry][name]) )
-        print("\t(%s, %s)," %(entry,outputD[entry][name] ) )
+        
+        print("\t(%s, %d)," %(entry, data) )
     print("\t]")
     plotD[name] = outputList
     plotD[name+'Time'] = time 
 
 # Output in Percent change from previous value
+firstVal = None
 print("# Data from: %s %s" %(dataDate, dataName))
 for name in outputTuples:
     print("%s = [ \n\t#(time , Precent change from previous value)" % name)
-    initVal = None
+    prevVal = None
+    firstCalc = True
     for entry in sorted(outputD):
-        if initVal == None:
-            initVal = float(outputD[entry][name])
-            prevVal = initVal
         
-        curVal = float(outputD[entry][name])
-        print("\t(%s, %.3f)," %(entry, (curVal-prevVal)/initVal*100 ) )
+        if firstVal == None:
+            # collect first value only once
+            firstVal = float(outputD[entry][name])
+            
+        if prevVal == None:
+            prevVal = float(outputD[entry][name])
+        
+        if firstCalc and firstValMatch:
+            curVal = firstVal
+            prevVal = firstVal
+            firstCalc = False
+        else:
+            curVal = float(outputD[entry][name])
+            
+        print("\t(%s, %.3f)," %(entry, (curVal-prevVal)/prevVal*100 ) )
         prevVal = curVal
         
     print("\t]")
 
-# make plot for fun
+# make plot
 import matplotlib.pyplot as plt
 fig, ax = plt.subplots()
 ax.plot(plotD['demandTime'],plotD['demand'] , label='Demand')
 ax.plot(plotD['forcastTime'],plotD['forcast'], label = 'Forcast')
 
-ax.set_title('Load Forcast and Demand on %s from %s'%(dataDate, dataName))
+ax.set_title('Load Forcast and Demand from %s\nStart Time = %s:00 %s '%
+             (dataName, dataTimeStart, dataDate))
 ax.set_ylabel('MW')
 ax.set_xlabel('Time [Hours]')
 ax.grid(True)
